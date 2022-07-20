fb7c34976f6218bb8d0d09ff91e3761a0c1d920f
subhanahmed <30408854+subhanahmed@users.noreply.github.com>
2022-07-19 23:53:15 -0700

    add request

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use(
             {url: /\/api\/v1\/products(.*)/, methods: ['GET','OPTIONS']},
             {url: /\/api\/v1\/categories(.*)/, methods: ['GET','OPTIONS']},
             '/api/v1/users/login',
-            '/api/v1/users/register'
+            '/api/v1/users/register',
+            '/api/v1/users'
         ] 
     })
   );
